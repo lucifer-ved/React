255df57efb2a44ff3db293349e6df346636e9d3b
Ved <ved.lucifer@gmail.com>
2021-05-19 03:46:47 +0530

    "Simple todo application with css, need to fix onclick css issue"

---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return
+        }
+        const newTodos = [todo, ...todos]
+        setTodos(newTodos);
+    }
+
+    const completeTodo  = id => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    }
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        }
+        const newTodos = [newValue, ...todos]
+        setTodos(prev => prev.map(item => (item.id === todoId  ? newValue : item )));
+    }
+
+    const removeTodo = id => {
+        const removeTodos = [...todos].filter(todo => todo.id  !== id)
+        setTodos(removeTodos);
+    }
+
+    return (
+        <div>
+            <h1> Let's plan your day !!</h1>
+            <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            updateTodo={updateTodo}/>
+        </div>
+    )
+}
+
+export default TodoList;
